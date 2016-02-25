f620630456a9c0e639d5ddde23d83bd33ba5b61c
Mathew Hall <mathew.james.hall@gmail.com>
2016-02-25 15:26:54 +0000

    Run scripts in /docker-entrypoint-init.d

--- shiny-server.sh
+++ shiny-server.sh
@@ -4,4 +4,11 @@
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
+for f in /docker-entrypoint-init.d/*; do
+	case "$f" in
+		*.sh)	echo "$0: running $f"; . "$f" ;;
+		*.R)	echo "$0: running $f"; R -f "$f" ;;
+	esac
+done
+
 exec shiny-server >> /var/log/shiny-server.log 2>&1
